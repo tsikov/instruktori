fb162c07dbc9e6e335210552c8016cdfaab3424f
tsikov <tsikov@gmail.com>
2015-05-27 11:53:36 +0300

    Configure capistrano for deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,30 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'instruktori'
+set :repo_url, 'git@github.com:tsikov/instruktori.git'
+set :scm, :git
+set :format, :pretty
+set :pty, true
+set :linked_files,    %w(config/database.yml config/secrets.yml config/application.yml)
+set :linked_dirs,     %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
+set :keep_releases,   20
+set :rails_env,       'production'
+set :puma_init_active_record, true
+set :nginx_server_name, 'instruktori.info *.instruktori.info'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :server, ask('Enter the server:')
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  namespace :nginx do
+    desc 'Generate an Nginx configuration file'
+    task :config do
+      on roles(:web) do |role|
+        template_puma('nginx_conf', "#{shared_path}/#{fetch(:nginx_config_name)}_nginx.conf", role)
+      end
     end
   end
-
 end
+
+after 'deploy:check', 'deploy:nginx:config'
+after 'deploy:check', 'puma:config'
