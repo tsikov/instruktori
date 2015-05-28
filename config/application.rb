ab12c62743139f164ac4cdbd13640c092f864b65
tsikov <tsikov@gmail.com>
2015-05-28 18:03:02 +0300

    Use a local file for environment variables.

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,17 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Instruktori
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
 
     config.time_zone = 'Sofia'
 
@@ -27,3 +29,5 @@ module Instruktori
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+
